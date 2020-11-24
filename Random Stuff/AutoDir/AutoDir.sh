bcdceeec93b3f62fc06fd54319e00fc69edc9e31
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-11-24 11:42:50 +0100

    Now it ask for file tipe

--- Random Stuff/AutoDir/AutoDir.sh
+++ Random Stuff/AutoDir/AutoDir.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-declare -i n
+declare -i n extenstion
 echo Introduzca el número de carpetas que desea crear: 
 read n
+echo Introduzca la extenstion del archivo que se incluirá en la carpeta: 
+read 
 i=0
 while [  $i -lt $n ]; do
     let i+=1
     mkdir E$i
     cd E$i
-    touch E$i.py
+    touch E$i.${REPLY}
     cd ..
 done
